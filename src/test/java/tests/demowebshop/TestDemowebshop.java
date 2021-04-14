a3f908d102a3cfdc126125026ac01419aeca3c13
SmallSparkle <77972389+SmallSparkle@users.noreply.github.com>
2021-04-14 14:44:35 +0200

    added files cookie snd body

--- src/test/java/tests/demowebshop/TestDemowebshop.java
+++ src/test/java/tests/demowebshop/TestDemowebshop.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
+import static utils.UtilsFile.readStringFromFile;
 
 class TestDemowebshop extends TestBase {
 
@@ -15,10 +16,12 @@ class TestDemowebshop extends TestBase {
   @Tag("shop")
   @DisplayName("Уведомление по email")
   void notifyByMail() {
+    String cookie = readStringFromFile("./src/test/resources/cookie_demowebshop.txt");
+
     Response response =
             given()
                     .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                    .cookie("Nop.customer=dcd17cea-9b97-48ca-a934-8d8a9e9b2c62; __utmc=78382081; __utmz=78382081.1616779538.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); ARRAffinity=06e3c6706bb7098b5c9133287f2a8d510a64170f97e4ff5fa919999d67a34a46; __utma=78382081.1806003553.1616779538.1616828183.1617780150.3; nop.CompareProducts=CompareProductIds=72; NopCommerce.RecentlyViewedProducts=RecentlyViewedProductIds=16&RecentlyViewedProductIds=31&RecentlyViewedProductIds=15&RecentlyViewedProductIds=72; __atuvc=7%7C14; __atuvs=606d5e20655f900a006; __utmb=78382081.18.10.1617780150")
+                    .cookie(cookie)
                     .when()
                     .post("/productemailafriend/16")
                     .then()
@@ -34,11 +37,14 @@ class TestDemowebshop extends TestBase {
   @Tag("shop")
   @DisplayName("Добавить товар в корзину")
   void testAddCart() {
+    String cookie = readStringFromFile("./src/test/resources/cookie_dwsh_cart.txt");
+    String body = readStringFromFile("./src/test/resources/body_dwsh.txt");
+
     Response response =
             given()
                     .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                    .cookie("Nop.customer=dcd17cea-9b97-48ca-a934-8d8a9e9b2c62; __utmc=78382081; __utmz=78382081.1616779538.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); ARRAffinity=06e3c6706bb7098b5c9133287f2a8d510a64170f97e4ff5fa919999d67a34a46; __utma=78382081.1806003553.1616779538.1616828183.1617780150.3; nop.CompareProducts=CompareProductIds=72; __utmt=1; NopCommerce.RecentlyViewedProducts=RecentlyViewedProductIds=16&RecentlyViewedProductIds=31&RecentlyViewedProductIds=15&RecentlyViewedProductIds=72; __atuvc=4%7C14; __atuvs=606d5e20655f900a003; __utmb=78382081.15.10.1617780150")
-                    .body("product_attribute_16_5_4=14&product_attribute_16_6_5=15&product_attribute_16_3_6=19&product_attribute_16_4_7=44&product_attribute_16_8_8=22&addtocart_16.EnteredQuantity=1")
+                    .cookie(cookie)
+                    .body(body)
                     .when()
                     .post("/addproducttocart/details/16/1")
                     .then()
